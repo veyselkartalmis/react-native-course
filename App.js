9616046e0a6a0d1307665609f84c354e7f2cfc00
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-02-12 01:57:54 +0300

    styles added

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another peace of text!</Text>
+        <Text style={styles.dummyText}>Another peace of text!</Text>
       </View>
-      <Text>Hello World!</Text>
-      <Button title='Tap Me!'/>
+      <Text style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16}}>
+        Hello World!
+      </Text>
+      <Button title="Tap Me!" />
     </View>
   );
 }
@@ -15,8 +17,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderColor: "black",
+    borderWidth: 2,
+  }
 });
