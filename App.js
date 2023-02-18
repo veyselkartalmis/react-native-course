d4cc1ab701b48a50a0c2f597dd45f5c75d758379
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-02-18 20:07:27 +0300

    flexbox - a deep dive

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!"/>
-        <Button title="Add Goal"/>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of goals</Text>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50,
-  },
-
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "80%",
-    padding: 8,
-    marginRight: 8,
-  }
-});
