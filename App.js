90fe21df68f6db6264f8be1b98750df791439f9a
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-02-21 21:43:40 +0300

    handling taps with the pressable component

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ export default function App() {
         ]);
     }
 
+    function deleteGoalHandler() {}
+
     return (
         <View style={styles.appContainer}>
             <GoalInput onAddGoal={addGoalHandler} />
@@ -21,7 +23,12 @@ export default function App() {
                 <FlatList
                     data={courseGoals}
                     renderItem={(itemData) => {
-                        return <GoalItem text={itemData.item.text} />;
+                        return (
+                            <GoalItem
+                                text={itemData.item.text}
+                                onDeleteItem={deleteGoalHandler}
+                            />
+                        );
                     }}
                     keyExtractor={(item, index) => {
                         return item.id;
