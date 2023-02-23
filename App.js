6dfb567594ea1b2a787516ca313d5bfd911d3d73
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-02-24 00:19:46 +0300

    app finishing touches

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { StyleSheet, View, FlatList, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
@@ -32,37 +33,41 @@ export default function App() {
     }
 
     return (
-        <View style={styles.appContainer}>
-            <View style={styles.modalButton}>
-                <Button
-                    title="Add New Goal"
-                    color="#5e0acc"
-                    onPress={startAddGoalHandler}
+        <>
+            {/* Status bar theme settings */}
+            <StatusBar style="light" />
+            <View style={styles.appContainer}>
+                <View style={styles.modalButton}>
+                    <Button
+                        title="Add New Goal"
+                        color="#fff"
+                        onPress={startAddGoalHandler}
+                    />
+                </View>
+                <GoalInput
+                    visible={modalIsVisible}
+                    onAddGoal={addGoalHandler}
+                    onCancel={endAddGoalHandler}
                 />
+                <View style={styles.goalsContainer}>
+                    <FlatList
+                        data={courseGoals}
+                        renderItem={(itemData) => {
+                            return (
+                                <GoalItem
+                                    text={itemData.item.text}
+                                    id={itemData.item.id}
+                                    onDeleteItem={deleteGoalHandler}
+                                />
+                            );
+                        }}
+                        keyExtractor={(item, index) => {
+                            return item.id;
+                        }}
+                    />
+                </View>
             </View>
-            <GoalInput
-                visible={modalIsVisible}
-                onAddGoal={addGoalHandler}
-                onCancel={endAddGoalHandler}
-            />
-            <View style={styles.goalsContainer}>
-                <FlatList
-                    data={courseGoals}
-                    renderItem={(itemData) => {
-                        return (
-                            <GoalItem
-                                text={itemData.item.text}
-                                id={itemData.item.id}
-                                onDeleteItem={deleteGoalHandler}
-                            />
-                        );
-                    }}
-                    keyExtractor={(item, index) => {
-                        return item.id;
-                    }}
-                />
-            </View>
-        </View>
+        </>
     );
 }
 
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -35,10 +35,10 @@ function GoalInput(props) {
                 />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
-                        <Button title="Add Goal" onPress={addGoalHandler} />
+                        <Button title="Cancel" onPress={props.onCancel} color="#f31282"/>
                     </View>
                     <View style={styles.button}>
-                        <Button title="Cancel" onPress={props.onCancel} />
+                        <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
                     </View>
                 </View>
             </View>
@@ -59,11 +59,12 @@ const styles = StyleSheet.create({
 
     textInput: {
         borderWidth: 1,
-        borderColor: "#cccccc",
+        borderColor: "#e4d0ff",
+        backgroundColor: "#e4d0ff",
+        color: "#120438",
         borderRadius: 6,
         width: "100%",
-        padding: 8,
-        color: "#fff"
+        padding: 16,
     },
 
     buttonContainer: {
