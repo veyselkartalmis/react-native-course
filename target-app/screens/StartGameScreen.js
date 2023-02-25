d2d46ac560fca4a674ec2479ecd0dbf050771b52
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-02-25 16:35:38 +0300

    setting up our screen componenets

---
+++ target-app/screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+
+function StartGameScreen() {
+    return (
+        <View>
+            <TextInput />
+        </View>
+    );
+}
+
+export default StartGameScreen;
